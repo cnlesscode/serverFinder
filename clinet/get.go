55719fb7396ed312634a906ecf42097fa353f63b
GraceCode <5213606@qq.com>
2025-04-16 17:57:38 +0800

    v1.1.3

--- clinet/get.go
+++ clinet/get.go
@@ -1,41 +1,12 @@
 package client
 
 import (
-	"encoding/json"
-	"time"
-
 	"github.com/cnlesscode/gotool"
-	"github.com/gorilla/websocket"
+	"github.com/cnlesscode/gotool/request"
 )
 
 // 获取数据
-func Get(addr, mainKey string, callBack func(msg map[string]any)) {
-	tryCount := 0
-	// 初始化连接地址
-	url := "ws://" + addr + APIBaseURL + "get&mainKey=" + mainKey + "&addr=" + gotool.GetLocalIP()
-GetDataLoop:
-	// 建立连接
-	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
-	if err != nil {
-		// 失败重连
-		tryCount++
-		if tryCount > 3 {
-			return
-		}
-		time.Sleep(time.Second)
-		goto GetDataLoop
-	}
-	defer conn.Close()
-	// 监听消息
-	for {
-		_, messageByte, err := conn.ReadMessage()
-		if err != nil {
-			break
-		}
-		message := make(map[string]any)
-		if err := json.Unmarshal(messageByte, &message); err != nil {
-			break
-		}
-		callBack(message)
-	}
+func Get(addr, mainKey string) (string, error) {
+	url := "http://" + addr + APIBaseURL + "get&mainKey=" + mainKey + "&addr=" + gotool.GetLocalIP()
+	return request.GET(url, nil, nil)
 }
--- clinet/remove.go
+++ clinet/remove.go
@@ -1,35 +1,13 @@
 package client
 
 import (
-	"time"
-
 	"github.com/cnlesscode/gotool"
-	"github.com/gorilla/websocket"
+	"github.com/cnlesscode/gotool/request"
 )
 
 // 删除数据 [ 不需要保持连接 ]
 func Remove(addr, mainKey, sonKey string) {
-	tryCount := 0
 	// 初始化连接地址
-	url := "ws://" + addr + APIBaseURL + "remove&mainKey=" + mainKey + "&sonKey=" + sonKey + "&addr=" + gotool.GetLocalIP()
-GetDataLoop:
-	// 建立连接
-	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
-	if err != nil {
-		// 失败重连
-		tryCount++
-		if tryCount > 3 {
-			return
-		}
-		time.Sleep(time.Second)
-		goto GetDataLoop
-	}
-	defer conn.Close()
-	// 监听消息
-	for {
-		_, _, err := conn.ReadMessage()
-		if err != nil {
-			break
-		}
-	}
+	url := "http://" + addr + APIBaseURL + "remove&mainKey=" + mainKey + "&sonKey=" + sonKey + "&addr=" + gotool.GetLocalIP()
+	request.GET(url, nil, nil)
 }
